58c6d68ae9589411927ca31d57ec002fdb75639f
Miguel Cabrerizo <doncicuto@gmail.com>
2025-01-01 12:09:33 +0100

    fix: scp command

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@
 site='/woodpecker/src/github.com/doncicuto/openuem-docs'
 apt-get update -y
 apt-get install sshpass -y
-sshpass -p $SFTP_PASS scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $site/build/ $SFTP_USER@$SFTP_HOST:/home/$SFTP_USER/docs/
+sshpass -p $SFTP_PASS scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $site/build/* $SFTP_USER@$SFTP_HOST:/home/$SFTP_USER/docs/
