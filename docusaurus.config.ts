f1150f79319b87dec3c3f6fa90dee37f882b8a6f
Miguel Cabrerizo <doncicuto@gmail.com>
2025-01-04 15:43:22 +0100

    feat: add sitemap.xml generation

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,6 +58,18 @@ const config: Config = {
         theme: {
           customCss: "./src/css/custom.css",
         },
+        sitemap: {
+          lastmod: 'date',
+          changefreq: 'daily',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+          createSitemapItems: async (params) => {
+            const {defaultCreateSitemapItems, ...rest} = params;
+            const items = await defaultCreateSitemapItems(rest);
+            return items.filter((item) => !item.url.includes('/page/'));
+          },
+        },
       } satisfies Preset.Options,
     ],
   ],
