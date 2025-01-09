f20c13f8fa04a7a957014ec753c09ccf16754f61
Miguel Cabrerizo <doncicuto@gmail.com>
2025-01-09 17:11:27 +0100

    fix: update repo location

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-site='/woodpecker/src/github.com/doncicuto/openuem-docs'
+site='/woodpecker/src/github.com/open-uem/openuem-docs'
 apt-get update -y
 apt-get install sshpass -y
 sshpass -p $SFTP_PASS scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $site/build/* $SFTP_USER@$SFTP_HOST:/home/$SFTP_USER/docs/
