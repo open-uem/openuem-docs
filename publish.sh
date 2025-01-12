837b08ba3068be180d160dac4ac60ce733335844
Miguel Cabrerizo <doncicuto@gmail.com>
2025-01-12 10:12:46 +0100

    fix: build sitemap location

--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@
 site='/woodpecker/src/github.com/open-uem/openuem-docs'
 apt-get update -y
 apt-get install sshpass -y
-gzip -c $site/sitemap.xml > $site/sitemap.xml.gz
+gzip -c $site/build/sitemap.xml > $site/build/sitemap.xml.gz
 sshpass -p $SFTP_PASS scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $site/build/* $SFTP_USER@$SFTP_HOST:/home/$SFTP_USER/docs/
