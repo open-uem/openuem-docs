49168df22c67fce0a458b448bf0aae3973cc546a
Miguel Cabrerizo <doncicuto@gmail.com>
2025-02-17 16:44:19 +0100

    feat: add default Algolia DocSearch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -147,6 +147,36 @@ const config: Config = {
       phpLoader: 'matomo.php',
       jsLoader: 'matomo.js',
     },
+    algolia: {
+      // The application ID provided by Algolia
+      appId: 'YLKH48EMAE',
+
+      // Public API key: it is safe to commit it
+      apiKey: '7c6e0e9f40336cb85a96413a04d85663',
+
+      indexName: 'openuem',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: '/docs/', // or as RegExp: /\/docs\//
+        to: '/',
+      },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
+    },
   } satisfies Preset.ThemeConfig,
 };
 
